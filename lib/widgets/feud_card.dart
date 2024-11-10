7f0b5229bb80a72e0175eb8cb6bd142bb74a96d8
Röttgen, Valentin <valentin.roettgen@adesso-finland.fi>
2024-11-10 17:47:20 +0200

    use icons instead of text in feudcard

--- lib/widgets/feud_card.dart
+++ lib/widgets/feud_card.dart
@@ -11,8 +11,29 @@ class FeudCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
         child: ListTile(
-            title: Text(feud.name),
-            subtitle: Text(
-                "Foes: ${feud.foes.length}, Skirmishes: ${feud.skirmishes.length}")));
+      title: Text(feud.name),
+      contentPadding: const EdgeInsets.all(16.0),
+      subtitle: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+        Tooltip(
+            message: "Foes",
+            child: Row(children: [
+              const Icon(Icons.people),
+              const SizedBox(width: 4),
+              Text(
+                feud.foes.length.toString(),
+              ),
+            ])),
+        const SizedBox(width: 8),
+        Tooltip(
+            message: "Skirmishes",
+            child: Row(children: [
+              const Icon(Icons.sports_mma),
+              const SizedBox(width: 4),
+              Text(
+                feud.skirmishes.length.toString(),
+              ),
+            ]))
+      ]),
+    ));
   }
 }
